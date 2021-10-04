a7ed9758df85ed79d34ae0a8c30ba4c0744f5b19
Chabla <Chabijohnthebaptiste@gmail.com>
2021-10-04 11:32:40 +0000

    facebook application

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+    has_many :posts, dependent: :destroy
+
+
+    before_validation { email.downcase! }
+    validates :name,  presence: true, length: { maximum: 30 }
+    validates :email, presence: true, uniqueness: true, length: { maximum: 255 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+    has_secure_password
+    validates :password, length: { minimum: 6 }
+
+    def to_s
+     name
+    end
+end
