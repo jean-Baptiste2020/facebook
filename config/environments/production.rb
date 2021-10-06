b0db030d3d0b4d20b43d37f43b4e7bf0ad2ab721
Chabla <Chabijohnthebaptiste@gmail.com>
2021-10-06 15:08:53 +0000

    configuration de l'envoi de mail en production avec smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.googlemail.com',
+  port:                 465,
+  domain:               'fbbk16@gmail.com',
+  password:             'bdzigkikpvozwoxt',
+  authentication:       'ssl',
+  enable_starttls_auto: true }
+
 end
